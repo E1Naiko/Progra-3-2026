270e5e4c5b9e32887d46664733374ccb09288f24
Nicolás Peñalba <matador50200@gmail.com>
2026-04-06 10:54:04 -0300

    Create BinaryTree.java

---
+++ tp2/ejercicio1/BinaryTree.java
@@ -0,0 +1,112 @@
+package tp2.ejercicio1;
+
+
+
+public class BinaryTree <T> {
+	
+	private T data;
+	private BinaryTree<T> leftChild;   
+	private BinaryTree<T> rightChild; 
+
+	
+	public BinaryTree() {
+		super();
+	}
+
+	public BinaryTree(T data) {
+		this.data = data;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+	/**
+	 * Preguntar antes de invocar si hasLeftChild()
+	 * @return
+	 */
+	public BinaryTree<T> getLeftChild() {
+		return leftChild;
+	}
+	/**
+	 * Preguntar antes de invocar si hasRightChild()
+	 * @return
+	 */
+	public BinaryTree<T> getRightChild() {
+		return this.rightChild;
+	}
+
+	public void addLeftChild(BinaryTree<T> child) {
+		this.leftChild = child;
+	}
+
+	public void addRightChild(BinaryTree<T> child) {
+		this.rightChild = child;
+	}
+
+	public void removeLeftChild() {
+		this.leftChild = null;
+	}
+
+	public void removeRightChild() {
+		this.rightChild = null;
+	}
+
+	public boolean isEmpty(){
+		return (this.isLeaf() && this.getData() == null);
+	}
+
+	public boolean isLeaf() {
+		return (!this.hasLeftChild() && !this.hasRightChild());
+
+	}
+		
+	public boolean hasLeftChild() {
+		return this.leftChild!=null;
+	}
+
+	public boolean hasRightChild() {
+		return this.rightChild!=null;
+	}
+	@Override
+	public String toString() {
+		return this.getData().toString();
+	}
+
+	public  int contarHojas() {
+		int res = contador(this, 0);
+		return res;
+	}
+		
+	private	int contador(BinaryTree<T> arbol, int act){
+		int res = 0;
+		if (arbol.hasLeftChild()){
+			res += contador(arbol.leftChild, act+1);
+		}
+		if (arbol.hasRightChild()){
+			res += contador(arbol.rightChild, act+1);
+		}
+		return act + res;
+	}
+    	 
+    public BinaryTree<T> espejo(){
+		BinaryTree<T> arbol = new BinaryTree<T>();
+
+
+ 	    return null;
+    }
+
+	private void reflejar(BinaryTree<T> rama){
+		r
+	}
+
+	// 0<=n<=m
+	public void entreNiveles(int n, int m){
+		
+   }
+		
+}
+
