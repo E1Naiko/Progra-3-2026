d45a0f8e79c361affa6ed3d864534650d4a4c216
Nicolás Peñalba <matador50200@gmail.com>
2026-04-24 17:49:54 -0300

    merequetengue

--- Tp3/GeneralTree.java
+++ Tp3/GeneralTree.java
@@ -1,6 +1,9 @@
 package tp3;
 
 import java.util.List;
+
+import Tp1.Ej8.Queue;
+
 import java.util.LinkedList;
 
 
@@ -59,6 +62,86 @@ public class GeneralTree<T> {
 		}
 	}
 
-	
+	public int altura(){
+		return recorrerArbolDevuelveAltura(this, 0);
+	}
 
+	private int recorrerArbolDevuelveAltura(GeneralTree<T> nodo, int altAct){
+		int may = 0, aux;
+
+		if (nodo.hasChildren()){
+			for (GeneralTree<T> child: nodo.getChildren()){
+				aux = recorrerArbolDevuelveAltura(child, altAct+1);
+				if (aux>may)
+					may = aux;
+			}
+		}
+
+		return (altAct > may) ? altAct : may;
+	}
+
+	public int nivel(T dato){
+		return recorrerArbolDevuelveNivel(this, dato);
+	}
+
+	private int recorrerArbolDevuelveNivel(GeneralTree<T> arbol, T dato){
+		int nivel = 0;
+
+		if (arbol == null || arbol.isEmpty())
+			return -1;
+
+		if (!arbol.getData().equals(dato)){
+			boolean encontre = false;
+			Queue<GeneralTree<T>> cola = new Queue<GeneralTree<T>>();
+			cola.enqueue(arbol);
+
+			while (!cola.isEmpty() && !encontre){
+				GeneralTree<T> nodo = cola.dequeue();
+				if (nodo.getData().equals(dato)){
+					encontre = true;
+				}
+
+				if (nodo.hasChildren()){
+					for(GeneralTree<T> child : nodo.getChildren()){
+						cola.enqueue(child);
+					}
+				}
+			}
+			nivel++;
+		}
+		return nivel;
+	}
+
+	public int ancho (){
+		return recorrerArbolDevuelveAncho(this)
+	}
+
+	private int recorrerArbolDevuelveAncho(GeneralTree<T> arbol){
+		int res = 0;
+		int ancho = 0;
+
+		if (arbol == null || arbol.isEmpty())
+			return -1;
+
+		Queue<GeneralTree<T>> cola = new Queue<GeneralTree<T>>();
+		cola.enqueue(arbol);
+		while (!cola.isEmpty()){
+			GeneralTree<T> nodo = cola.dequeue();
+			if (nodo.hasChildren()){
+				for(GeneralTree<T> child : nodo.getChildren()){
+					cola.enqueue(child);
+					ancho++;
+				}
+			}
+			else {
+				if (!cola.isEmpty()){
+					if (ancho>res)
+						res = ancho;
+					ancho = 0;
+					cola.enqueue(null);
+				}
+			}
+		}
+		return res;
+	}
 }
