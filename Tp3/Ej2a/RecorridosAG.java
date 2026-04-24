ea64eefbb4ce7ca0fd3557c0a1d8437667eaf6de
Nicolás Peñalba <matador50200@gmail.com>
2026-04-24 16:02:37 -0300

    Update RecorridosAG.java

--- Tp3/Ej2a/RecorridosAG.java
+++ Tp3/Ej2a/RecorridosAG.java
@@ -4,10 +4,12 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import Tp1.Ej8.Queue;
+import ejercicio1.BinaryTree;
 import tp3.GeneralTree;
 
 public class RecorridosAG {
-    public List<Integer> numerosImparesMayoresQuePreOrden (Tp3.Ej1.GeneralTree<T> <Integer> a, Integer n){
+    public List<Integer> numerosImparesMayoresQuePreOrden (GeneralTree<Integer> a, Integer n){
         List<Integer> res = new LinkedList<>();
         
         if (a!=null && !a.isEmpty())
@@ -23,7 +25,7 @@ public class RecorridosAG {
                 res.add(act);
 
             if (nodo.hasChildren()){
-                List<Integer> children = nodo.getChildren();
+                List<GeneralTree<Integer>> children = nodo.getChildren();
                 for (GeneralTree<Integer> child: children){
                     preOrder(child, n, res);
                 }
@@ -60,23 +62,26 @@ public class RecorridosAG {
     public List<Integer> numerosImparesMayoresQueInOrden (GeneralTree <Integer> a, Integer n){
         List<Integer> res = new LinkedList<>();
         
-        if (a!=null && !a.isEmpty())
+        if (a!=null && !a.isEmpty()){
             inOrder(a, n, res);
-
-        return res;
+            return res;
+        }
+        return 0;
     }
     
     private void inOrder(GeneralTree<Integer> nodo, Integer n, List<Integer> res){
         if (nodo!=null){
-            List<Integer> children;
-            Iterator<GeneralTree<Integer>> it;
+            List<GeneralTree<Integer>> children;
+            Iterator<GeneralTree<Integer>> it = null;
             GeneralTree<Integer> child;
             
             if (nodo.hasChildren()){
                 children = nodo.getChildren();
                 it = children.iterator();
-                child = it.next();
-                inOrder(child, n, res);
+                if (it.hasNext()){
+                    child = it.next();
+                   inOrder(child, n, res);
+                }
             }
             
             int act = nodo.getData();
@@ -102,12 +107,83 @@ public class RecorridosAG {
         return res;
     }
 
+    private void postOrder(GeneralTree<Integer> nodo, Integer n, List<Integer> res){
+        if (nodo!=null){            
+            if (nodo.hasChildren()){
+                List<GeneralTree<Integer>> children = nodo.getChildren();
+                for (GeneralTree<Integer> child: children){
+                    postOrder(child, n, res);
+                }
+            }
+
+            int act = nodo.getData();
+            if (act % 2 != 0 && act > n.intValue())
+                res.add(act);
+        }
+    }
+
     public List<Integer> numerosImparesMayoresQuePorNiveles(GeneralTree <Integer> a, Integer n){
-        List<Integer> res = new LinkedList<>();
-        
         if (a!=null && !a.isEmpty())
+            LinkedList<Integer> res = porNiveles(a, n);
+
+        return res;
+    }
+
+    private List<Integer> porNiveles(GeneralTree <Integer> a, Integer n){
+        int aux;
+        List<Integer> res = new LinkedList<>();
+        Queue<GeneralTree<Integer>> cola = new Queue<GeneralTree<Integer>>();
+        cola.enqueue(a);
+
+        while (!cola.isEmpty()){
+			GeneralTree<Integer> nodo = cola.dequeue();
+
+            aux = nodo.getData();
+			if (aux % 2 != 0 && aux > n.intValue()){
+                res.add(aux);
+            }
+
+            if (nodo.hasChildren())
+			    for (GeneralTree<Integer> child : nodo.getChildren()){
+                    cola.enqueue(child);
+                }
+
+			else if (!cola.isEmpty()) {
+                cola.enqueue(null);
+            }
+		}
+
+        return res;
+    }
+
+    /*
+    public List<Integer> numerosImparesMayoresQuePorNiveles(GeneralTree<Integer> a, Integer n){
+        List<Integer> res = new LinkedList<>();
+
+        if (a != null && !a.isEmpty())
             porNiveles(a, n, res);
 
         return res;
     }
+
+    private void porNiveles(GeneralTree<Integer> a, Integer n, List<Integer> res){
+        Queue<GeneralTree<Integer>> cola = new Queue<>();
+        cola.enqueue(a);
+
+        while (!cola.isEmpty()){
+            GeneralTree<Integer> nodo = cola.dequeue();
+
+            int aux = nodo.getData();
+            if (aux % 2 != 0 && aux > n){
+                res.add(aux);
+            }
+
+            if (nodo.hasChildren()){
+                for (GeneralTree<Integer> child : nodo.getChildren()){
+                    cola.enqueue(child);
+                }
+            }
+        }
+    }
+     */
 }
